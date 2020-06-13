c3ab1921077f565818700f48727d22350f77f5f2
felipe Januario <felipejanuario86@gmail.com>
2020-06-13 04:35:41 -0300

    Método para busca de aluno por ID na base de dados

--- src/main/java/part3/QueriesExecution.java
+++ src/main/java/part3/QueriesExecution.java
@@ -1,19 +1,31 @@
 package part3;
-import java.util.List;
-
-
 public class QueriesExecution {
     public static void main(String[] args) {
-        try {
-            AlunoDAO alunoDAO = new AlunoDAO();
+        AlunoDAO alunoDAO = new AlunoDAO();
+       /* try {
 
-            List<Aluno> alunos = alunoDAO.list();
+                List<Aluno> listaDeAlunos = alunoDAO.list();
 
-            alunos.stream().forEach(System.out::println);
-        }catch (Exception e){
-            System.out.println("Erro ao consultar a lista");
+            listaDeAlunos.stream().forEach(System.out::println);
+        }catch (Exception e) {
+                System.out.println("Erro ao consultar a lista");
+                e.printStackTrace();
+        }*/
+
+        //Consulta Aluno por ID
+        try{
+
+            Aluno alunoParaConsulta = alunoDAO.getById(2);
+            System.out.println(alunoParaConsulta);
+        }catch(Exception e){
+            System.out.println("Erro ao consultar Aluno por ID");
             e.printStackTrace();
-        }
-    }
 
+        }
+
+
+    }
 }
+
+
+
