d8f4b7565627389242246b948939127ab94b5bde
felipe Januario <felipejanuario86@gmail.com>
2020-06-13 04:16:57 -0300

    Criando arquivo connection.properties

--- src/main/java/part3/QueriesExecution.java
+++ src/main/java/part3/QueriesExecution.java
@@ -1,4 +1,19 @@
 package part3;
+import java.util.List;
+
 
 public class QueriesExecution {
+    public static void main(String[] args) {
+        try {
+            AlunoDAO alunoDAO = new AlunoDAO();
+
+            List<Aluno> alunos = alunoDAO.list();
+
+            alunos.stream().forEach(System.out::println);
+        }catch (Exception e){
+            System.out.println("Erro ao consultar a lista");
+            e.printStackTrace();
+        }
+    }
+
 }
