29b18039df7b3be0e79ea073aae66695fbe9d132
felipe Januario <felipejanuario86@gmail.com>
2020-06-13 06:51:29 -0300

    Criando a classe executionPart1, para rodar a transação de persistencia no BD

--- src/main/java/classes/Estado.java
+++ src/main/java/classes/Estado.java
@@ -23,4 +23,59 @@ public class Estado {
             orphanRemoval = true
     )
     private List<Aluno> alunos = new ArrayList<>();
+    public Estado() { }
+
+    public Estado(String nome, String sigla) {
+        this.nome = nome;
+        this.sigla = sigla;
+    }
+
+    public Estado(String nome, String sigla, List<Aluno> alunos) {
+        this.nome = nome;
+        this.sigla = sigla;
+        this.alunos = alunos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    @Override
+    public String toString() {
+        return "Estado{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", sigla='" + sigla + '\'' +
+                ", alunos=" + alunos +
+                '}';
+    }
+
 }
---
+++ src/main/java/part1/ExecutionPart1.java
@@ -0,0 +1,37 @@
+package part1;
+
+import classes.Aluno;
+import classes.Estado;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ExecutionPart1 {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("part1-DIO");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Estado estadoParaAdicionar = new Estado("Rio de Janeiro", "RJ");
+        Aluno alunoParaAdicionar = new Aluno("Felipe",33,estadoParaAdicionar);
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(estadoParaAdicionar);
+        entityManager.persist(alunoParaAdicionar);
+
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+
+
+
+    }
+
+
+
+}
